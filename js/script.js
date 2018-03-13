1db4bee382610831da0dcac51e866291c6f09b4d
Olga Kozhevnikova <oolya.kozhevnikova@gmail.com>
2018-03-13 14:57:17 +0400

    Third commit

--- js/script.js
+++ js/script.js
@@ -10,22 +10,31 @@ $(() => {
 			$('#value').append(balance);
 			let initialVal = balance*100/15; //a percentage of $14 out of $15
 			$('.bar-step').css('left', initialVal + '%');
-		}
-	})
+			activateBar();
+		},
+		error: (xhr,status,error) => {
+			console.log('error');
+			$('#value').append('0');
+			$('#required-amount').text('15');
 
-	let current_progress = 0;
-	let interval = setInterval(() => {
-		current_progress += 0.2*100/15; //a percentage of $0.2 out of $15
-		$("#dynamic").css("width", current_progress + "%").attr("aria-valuenow", current_progress);
-		if (current_progress >= 100) {
-		  clearInterval(interval);
-		  changeColor();
 		}
-	}, 2000);
+	});
 
-	const changeColor = () => {
-		$('#target').css('background', '#00A910');
-		$('hr').css('border-bottom', '#fff');
-		$('#reference').hide();
+	let activateBar = () => {
+		let current_progress = 0;
+		let interval = setInterval(() => {
+			current_progress += 0.2*100/15; //a percentage of $0.2 out of $15
+			$("#dynamic").css("width", current_progress + "%").attr("aria-valuenow", current_progress);
+			if (current_progress >= 100) {
+				clearInterval(interval);
+				changeColor();
+			}
+		}, 2000);
+
+		const changeColor = () => {
+			$('#target').css('background', '#00A910');
+			$('hr').css('border-bottom', '#fff');
+			$('#reference').hide();
+		}
 	}
 });
